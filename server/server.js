107c1de53a42f6f68afd70d39d099d0111bf7f5c
Che' Nxusani <che.nxusani@gmail.com>
2016-10-04 12:43:57 +0200

    file size

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
 app.use(fileUpload());
 
 var NODE_ENV = process.env.NODE_ENV || 'development';
