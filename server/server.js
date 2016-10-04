3af9cf9d83585e373f23c5a4bc8c0e3106ca4b60
Che' Nxusani <che.nxusani@gmail.com>
2016-10-04 14:51:51 +0200

    updated resize behaviour

--- server/server.js
+++ server/server.js
@@ -198,7 +198,7 @@ app.post(`/cms/api/section/:id/add-image`, function(req, res) {
         var dimensions = sizeOf(finalFilePath);
 
         Jimp.read(finalFilePath).then(function (image) {
-          image.resize(88, 100)
+          image.resize(Jimp.AUTO, 100)
            .write(path.join(UPLOAD_PATH, `${fileId}_thumb${fileExt}`));
 
            PortfolioImages.insert({
